8b951c8562f28a2ce0b1a39a31799e5dd0966da6
Ana Sequeira <anacseq@gmail.com>
2026-01-29 20:56:15 +0000

    remove secret

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y!o73e7&o(#y7ye_o^1@^shqlwd*iczn*0sc2$^8wdwwhjvp!j'
+SECRET_KEY = 'django-insecure-xxxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
